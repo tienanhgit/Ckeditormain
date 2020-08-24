98e82bda4538f82a3a5c31dabf0f3a9ae467965a
Tiến anh Bùi <minhanhtienanh@gmail.com>
2020-08-24 14:49:42 +0700

    Add project files.

---
+++ CKeditor/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace CKeditor.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult Index()
+        //{
+        //    ViewBag.picture = Picture;
+        //    ViewBag.detail = Detail;
+        //    return View("Detail");
+        //}
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public  ActionResult Detail(string Picture, string Detail)
+        {
+            ViewBag.picture = Picture;
+            ViewBag.detail = Detail;
+            return View();
+
+        }
+    }
+}
---
+++ CKeditor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CKeditor.MvcApplication" Language="C#" %>
